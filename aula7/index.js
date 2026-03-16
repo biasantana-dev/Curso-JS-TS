d7aa6ccb3ae2cc83f8089b7f24cec15dc46d068d
Bia Santana <beatrizsantana0914@gmail.com>
2026-03-16 20:35:17 -0300

    Update index.js

---
+++ aula7/index.js
@@ -0,0 +1,13 @@
+// Não podemos criar constantes com palavras reservadas 
+//constantes precisam ter nomes significativos
+// Não pode começar o nome de uma constantes com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive (letras maiúsculas e minúsculas fazem a diferença)
+// Não pode modificar o valor de uma constante
+// NÃO UTILIZE VAR, UTILIZE CONST.
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
