037ad72ad6ff95bab5d3cd44a83764ba5e17a150
Bia Santana <beatrizsantana0914@gmail.com>
2026-03-17 19:35:18 -0300

    update aula 9

--- aula9/index.js
+++ aula9/index.js
@@ -1,4 +1,17 @@
-var nome = 'Luiz';
-var nome = 'Otávio';
+// String, number, undefined, null, boolean, symbol
 
-console.log(nome);
+const nome = 'Luiz'; // string
+const nome1 = "Luiz"; // string
+const nome2 = `Luiz`; // string
+const num1 = 10; // number 
+const num2 = 10.52; // number 
+let nomeAluno; // undefined -> não aponta pra local nenhum na nenhum na memória
+const sobrenomeAluno = null; // nulo -> não aponta pra local nenhum na memória
+const aprovado = false; // Boolean = true, false (lógico)
+
+let a = 2; 
+const b = a;
+console.log(a, b); // 2, 2
+
+a = 3; 
+console.log(a, b); // 3, 2
