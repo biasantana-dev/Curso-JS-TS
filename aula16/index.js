fa9aacb3f966805efca1bcc6147eb983853d5b6a
Bia Santana <beatrizsantana0914@gmail.com>
2026-03-24 16:52:24 -0300

    Create index.js

---
+++ aula16/index.js
@@ -0,0 +1,41 @@
+const alunos = ['Luiz', 'Maria', 'João']; // Luiza, Eduardo
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+// alunos.push('Luiza');
+// alunos.push('Eduardo');
+// console.log(alunos.slice(0, -2));
+
+
+// console.log(alunos[50]);
+
+
+// delete alunos[1];
+// console.log(alunos);
+
+
+// const removido = alunos.shift();
+// console.log(removido);
+// console.log(alunos);
+
+// alunos.unshift('Luiza'); // Adiociona no começo
+// alunos.unshift('Fábio');
+
+
+// alunos.push('Daniel');  // Adiciona no fim 
+// alunos.push('Mateus');
+
+
+// alunos[alunos.length] = 'Luiza'; // Adiciona no fim 
+// alunos[alunos.length] = 'Pedro';
+// alunos[alunos.length] = 'Ana';
+
+
+// alunos[0] = 'Eduardo'; // Altera 
+// alunos[3] = 'Luiza'; // Adicionando
+
+
+// console.log(alunos);
+// console.log(alunos[0]);
+// console.log(alunos[2]);
