5b6f7a23f73c16caf5bc5974517fcd5e0a992206
Bia Santana <beatrizsantana0914@gmail.com>
2026-03-25 19:15:11 -0300

    Create index.js

---
+++ aula19/index.js
@@ -0,0 +1,15 @@
+/*
+Primitivos (imutáveis) - string, number, boolean, undefined, null (bigint, symbol) - Valor
+
+Referência (mutável) - array, object, function 
+ */
+
+let a = [1, 2, 3];
+let b = a;
+console.log(a, b);
+
+a.push(4);
+console.log(a, b);
+
+b.pop();
+console.log(a, b);
