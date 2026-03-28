a59c87087680410c4d685e9c7b92254e511e81a7
Bia Santana <beatrizsantana0914@gmail.com>
2026-03-28 18:19:16 -0300

    Create index.js

---
+++ aula22/index.js
@@ -0,0 +1,11 @@
+//  const expressaoAnd = true && true && false;
+//  const expressaoOr =  true || false;
+
+// const usuario = 'Luiz';
+// const senha = '123456';
+
+// const vaiLogar = usuario === 'Luiz' && senha === '123455';
+// console.log(vaiLogar);
+
+
+console.log(!true);
