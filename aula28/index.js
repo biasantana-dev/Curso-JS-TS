eaa3d7ecc8475dc77d503d03523f3c10fa8c4c0c
Bia Santana <beatrizsantana0914@gmail.com>
2026-04-08 19:38:49 -0300

    Update index.js

--- aula28/index.js
+++ aula28/index.js
@@ -1,2 +1,12 @@
-const data = new Date();
+// const tresHotas = 60 * 60 * 3 * 1000;
+// const data = new Date(2019, 3); // a, m, d, h, M, s, ms
+const data = new Date('2019-04-20 20:20:59')
+console.log('Dia', data.getDate());
+console.log('Mês', data.getMonth());
+console.log('Ano', data.getFullYear());
+console.log('Hora', data.getHours());
+console.log('Min', data.getMinutes());
+console.log('Seg', data.getSeconds());
+console.log('Ms', data.getMilliseconds());
+console.log('Dia semana', data.getDay());
 console.log(data.toString());
