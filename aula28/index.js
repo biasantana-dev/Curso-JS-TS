f9e54f9b9bf9a2768e09841d08501d578405d6d7
Bia Santana <beatrizsantana0914@gmail.com>
2026-04-09 20:56:49 -0300

    aula 30

--- aula28/index.js
+++ aula28/index.js
@@ -1,12 +1,16 @@
 // const tresHotas = 60 * 60 * 3 * 1000;
 // const data = new Date(2019, 3); // a, m, d, h, M, s, ms
-const data = new Date('2019-04-20 20:20:59')
-console.log('Dia', data.getDate());
-console.log('Mês', data.getMonth() + 1);
-console.log('Ano', data.getFullYear());
-console.log('Hora', data.getHours());
-console.log('Min', data.getMinutes());
-console.log('Seg', data.getSeconds());
-console.log('Ms', data.getMilliseconds());
-console.log('Dia semana', data.getDay());
-console.log(data.toString());
+// const data = new Date('2019-04-20 20:20:59')
+// console.log('Dia', data.getDate());
+// console.log('Mês', data.getMonth() + 1);
+// console.log('Ano', data.getFullYear());
+// console.log('Hora', data.getHours());
+// console.log('Min', data.getMinutes());
+// console.log('Seg', data.getSeconds());
+// console.log('Ms', data.getMilliseconds());
+// console.log('Dia semana', data.getDay());
+// console.log(data.toString());
+
+function zeroAEsquerda (num) {
+   return num >= 10 ? num : `0${num}`;
+}
