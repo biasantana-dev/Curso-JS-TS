74623cba6c2fb5361b61f886f9e2d121135e6527
Bia Santana <beatrizsantana0914@gmail.com>
2026-04-13 19:37:06 -0300

    Create index.js

---
+++ aula31/index.js
@@ -0,0 +1,33 @@
+const verdadeira = true;
+ 
+// Let tem escopo de bloco {...}
+// Var só tem escopo de função
+
+// let nome = 'Luiz'; // criando 
+// var nome2 = 'Luiz'; // criando
+
+// if (verdadeira) {
+//    let nome = 'Otávio'; // criando
+//    var nome2 = 'Ana'; // recriando 
+
+//    if (verdadeira) {
+//       var nome2 = 'Pedro'; //Redeclarada
+//       let nome = 'Outra coisa'
+//    }
+// }
+
+
+// console.log(nome, nome2);
+
+// function falaOi () {
+//    if (verdadeira) {
+//       let nome = 'Luiz';
+//       var sobrenome = 'Miranda';
+//    }
+//    console.log(sobrenome);
+// }
+
+// falaOi(); 
+
+let sobrenome ='Miranda';
+console.log(sobrenome);
