2929f0292850f1221ca26e62ce96882d1479b37b
Bia Santana <beatrizsantana0914@gmail.com>
2026-04-18 20:25:38 -0300

    Update main.js

--- aula35/assets/js/main.js
+++ aula35/assets/js/main.js
@@ -1,16 +1,58 @@
 const elementos = [
-{tag: 'p', texto: 'Frase 1'},
-{tag: 'div', texto: 'Frase 2'},
-{tag: 'footer', texto: 'Frase 3'},
-{tag: 'section', texto: 'Frase 4'}
+   {tag: 'p', texto: 'Frase 1'},
+   {tag: 'div', texto: 'Frase 2'},
+   {tag: 'footer', texto: 'Frase 3'},
+   {tag: 'section', texto: 'Frase 4'}
 ];
 
 const container = document.querySelector('.container');
-injetHtml = '';
+const div = document.createElement('div');
 
 for (let i = 0; i < elementos.length; i++) {
    let {tag, texto} = elementos[i];
-   injetHtml += `<${tag}>${texto} </${tag}>`;
+   let tagCriada = document.createElement(tag);
+   tagCriada.innerText = texto;
+   
+   // let textoCriado = document.createTextNode(texto); // Node Texto
+   // tagCriada.appendChild(textoCriado);
+   div.appendChild(tagCriada);
 }
 
-container.innerHTML += `<div>${injetHtml}</div>`;
+container.appendChild(div);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const elementos = [
+// {tag: 'p', texto: 'Frase 1'},
+// {tag: 'div', texto: 'Frase 2'},
+// {tag: 'footer', texto: 'Frase 3'},
+// {tag: 'section', texto: 'Frase 4'}
+// ];
+
+// const container = document.querySelector('.container');
+// injetHtml = '';
+
+// for (let i = 0; i < elementos.length; i++) {
+//    let {tag, texto} = elementos[i];
+//    injetHtml += `<${tag}>${texto} </${tag}>`;
+// }
+
+// container.innerHTML += `<div>${injetHtml}</div>`;
+
