13835d18dbd370de0ccdbf787e4a3100c57287e9
Bia Santana <beatrizsantana0914@gmail.com>
2026-04-23 20:45:17 -0300

    Adicionar exemplos de iteração com for, for in e for of em arrays

---
+++ logica-de-programacao/aula36/index.js
@@ -0,0 +1,23 @@
+// For in -> lê os índices ou chaves do objeto
+//
+const pessoa = {
+   nome: 'Pedro',
+   sobrenome: 'Santana',
+   idade: 30
+};
+
+
+ 
+for (let chave in pessoa) {
+   console.log(chave, pessoa[chave]);
+}
+
+
+// for (let i in frutas) {
+//    console.log(frutas[i]);
+// }
+
+
+// for (let i = 0; i < frutas.length; i++) {
+//    console.log(frutas[i]);
+// }
