a81f820a46cd3df0361169dcdf2dfd098beba7f9
Bia Santana <beatrizsantana0914@gmail.com>
2026-04-27 15:49:54 -0300

    Adicionar tratamento de erros na função retornaHora e mensagem final

--- logica-de-programacao/aula45/index.js
+++ logica-de-programacao/aula45/index.js
@@ -1,7 +1,23 @@
+function retornaHora(data) {
+   if (data && !(data instanceof Date)) {
+      throw new TypeError('Esperando instância de Date.');
+   }
+
+   if (!data) {
+      data = new Date();
+   }
+
+   return data.toLocaleTimeString('pt-BR', {
+      hour12: false
+   })
+}
+
 try {
- // É executada quando há erros
+   const data = new Date('01-01-1970 12:58:12');
+   const hora = retornaHora();
+   console.log(hora);
 } catch (e) {
- // É executada quando há erros
+   // Tratar erro
 } finally {
-   // Sempre
+   console.log('Tenha um bpm dia!')
 }
