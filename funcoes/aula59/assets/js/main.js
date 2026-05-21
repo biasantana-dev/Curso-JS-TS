116d49aa6c6be429c23060030bc86b6b9b4d401e
Bia Santana <beatrizsantana0914@gmail.com>
2026-05-21 19:44:02 -0300

    Refatorar a classe Calculadora: renomear métodos e simplificar lógica de captura de eventos

--- funcoes/aula59/assets/js/main.js
+++ funcoes/aula59/assets/js/main.js
@@ -1,70 +1,51 @@
 function Calculadora() {
-   this.display = document.querySelector('.display'),
+   this.display = document.querySelector('.display');
 
    this.inicia = () => {
-      this.cliqueBotoes();
-      this.pressionaEnter();
+      this.capturaCliques();
+      this.capturaEnter();
    };
 
-   this.pressionaEnter = () => {
-      this.display.addEventListener('keyup', e => {
+   this.capturaEnter = () => {
+      document.addEventListener('keyup', e => {
          if (e.keyCode === 13) {
             this.realizaConta();
          }
       });
    };
 
-   this.clearDisplay = () => {
-      this.display.value = '';
-   };
-
-   this.apagaUm = () => {
-      this.display.value = this.display.value.slice(0, -1);
+   this.capturaCliques = () => {
+      document.addEventListener('click', event => {
+         const el = event.target;
+         if (el.classList.contains('btn-num')) this.addNumDisplay(el);
+         if (el.classList.contains('btn-clear')) this.clear();
+         if (el.classList.contains('btn-del')) this.del();
+         if (el.classList.contains('btn-eq')) this.realizaConta();
+      });
    };
 
    this.realizaConta = () => {
-      let conta = this.display.value;
-
       try {
-         conta = eval(conta);
+         const conta = eval(this.display.value);
 
          if (!conta) {
-            alert('Conta Inválida');
+            alert('Conta Inválida!');
             return;
          }
 
-         this.display.value = String(conta);
+         this.display.value = conta;
       } catch (e) {
-         alert('Conta Inválida');
+         alert('Conta Inválida!');
          return;
       }
    };
 
-   this.cliqueBotoes = () => {
-      document.addEventListener('click', e => {
-         const el = e.target;
-
-         if (el.classList.contains('btn-num')) {
-            this.btnParaDisplay(el.innerText);
-         }
-
-         if (el.classList.contains('btn-clear')) {
-            this.clearDisplay();
-         }
-
-         if (el.classList.contains('btn-del')) {
-            this.apagaUm();
-         }
-
-         if (el.classList.contains('btn-eq')) {
-            this.realizaConta();
-         }
-      });
+   this.addNumDisplay = el => {
+      this.display.value += el.innerText;
+      this.display.focus();
    };
-
-   this.btnParaDisplay = (valor) => {
-      this.display.value += valor;
-   };
+   this.clear = () => this.display.value = '';
+   this.del = () => this.display.value = this.display.value.slice(0, -1);
 }
 
 const calculadora = new Calculadora();
