ac5f3d5f2e74c1065c1a1a2ee4ceca19cf0bb805
Bia Santana <beatrizsantana0914@gmail.com>
2026-06-10 20:59:47 -0300

    Adicionar funções construtoras e métodos para manipulação de produtos e camisetas

---
+++ objetos/aula75.js
@@ -0,0 +1,42 @@
+function Produto(nome, preco) {
+   this.nome = nome;
+   this.preco = preco;
+}
+
+Produto.prototype.desconto = function (percentual) {
+   this.preco = this.preco - (this.preco * (percentual / 100));
+};
+
+Produto.prototype.aumento = function (percentual) {
+   this.preco = this.preco + (this.preco * (percentual / 100));
+};
+
+// Literal
+const p1 = new Produto('Camiseta', 50);
+
+const p2 = {
+   nome: 'Caneca',
+   preco: 15
+};
+
+Object.setPrototypeOf(p2, Produto.prototype);
+
+p2.aumento(10);
+
+const p3 = Object.create(Produto.prototype, {
+   preco: {
+      writable: true,
+      configurable: true,
+      enumerable: true,
+      value: 99
+   },
+   temanho2: {
+      writable: true,
+      configurable: true,
+      enumerable: true,
+      value: 42
+   }
+});
+
+p3.aumento(10);
+console.log(p3);
---
+++ objetos/aula76.js
@@ -0,0 +1,23 @@
+// Produto -> aumento, desconto
+// Camiseta = Cor, Caneca = Material
+function Produto(nome, preco) {
+   this.nome = nome;
+   this.preco = preco;
+}
+Produto.prototype.aumento = function (quantia) {
+   this.preco += quantia;
+};
+Produto.prototype.desconto = function (quantia) {
+   this.preco -= quantia;
+};
+
+function Camiseta(nome, preco, cor) {
+   Produto.call(this, nome, preco);
+}
+Camiseta.prototype = Object.create(Produto.prototype);
+Camiseta.prototype.constructor = Camiseta;
+
+const produto = new Produto('Gen', 111);
+const camiseta = new Camiseta('Regata', 7.5, 'preta');
+console.log(produto);
+console.log(camiseta);
