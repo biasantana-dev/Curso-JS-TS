abd6dc571c3b68a21e0585f0a717f3a7384ae74d
Bia Santana <beatrizsantana0914@gmail.com>
2026-06-11 20:14:26 -0300

    Adicionar propriedades e métodos para as classes Camiseta e Caneca

--- objetos/aula76.js
+++ objetos/aula76.js
@@ -13,11 +13,44 @@ Produto.prototype.desconto = function (quantia) {
 
 function Camiseta(nome, preco, cor) {
    Produto.call(this, nome, preco);
+   this.cor = cor;
 }
 Camiseta.prototype = Object.create(Produto.prototype);
 Camiseta.prototype.constructor = Camiseta;
 
+Camiseta.prototype.aumento = function (percentual) {
+   this.preco = this.preco + (this.preco * (percentual / 100));
+};
+
+function Caneca(nome, preco, material, estoque) {
+   Produto.call(this, nome, preco);
+   this.material = material;
+
+   Object.defineProperty(this,  'estoque', {
+      enumerable: true,
+      configurable: false,
+      get: function() {
+         return estoque;
+      },
+      set: function(valor) {
+         if (typeof valor !== 'number') return;
+         estoque = valor;
+      }
+   });
+   
+}
+
+Caneca.prototype = Object.create(Produto.prototype);
+Caneca.prototype.constructor = Caneca;
+
+
 const produto = new Produto('Gen', 111);
 const camiseta = new Camiseta('Regata', 7.5, 'preta');
-console.log(produto);
+const caneca = new Caneca('Caneca', 13, 'porcelana', 5);
+caneca.estoque = 100;
+
+console.log(caneca.estoque);
+console.log(caneca);
 console.log(camiseta);
+console.log(produto);
+
