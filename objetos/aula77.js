2bbce535ff7d8bd681f26b2c9cd96cf68b9fcd65
Bia Santana <beatrizsantana0914@gmail.com>
2026-06-19 19:08:06 -0300

    Update aula77.js

--- objetos/aula77.js
+++ objetos/aula77.js
@@ -16,6 +16,28 @@ se o digito for maior que 9, consideramos 0.
 se o digito for maior que 9, consideramos 0.
 */
 
-let cpf = '705.484.450-52';
-let cpfLimpo = cpf.replace(/\D+/g, '');
-console.log(cpfLimpo);
+function ValidaCPF(cpfEnviado) {
+   Object.defineProperty(this, 'cpfLimpo', {
+      enumerable: true,
+      get: function() {
+         return cpfEnviado.replace(/\D+/g, '');
+      }
+   });
+}
+
+ValidaCPF.prototype.valida = function() { 
+   if (typeof this.cpfLimpo === 'undefined') return false;
+   if (this.cpfLimpo.length !== 11) return false;
+
+   const cpfParcial = this.cpfLimpo.slice(0, -2);
+   const digito1 = this.criaDigito();
+   return true;
+};
+
+ValidaCPF.prototype.criaDigito = function(cpfParcial) {
+   const cpfArray = Array.from(cpfParcial);
+   console.log(cpfArray);
+};
+
+const cpf = new ValidaCPF('705.484.450-52');
+console.log(cpf.valida());
